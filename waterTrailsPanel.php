990a6cc892821c1ef4d4a4a13bd585652a687d6d
Ford Peterson <fordpeterson@gmail.com>
2011-11-14 10:57:25 -0500

    new file:   combined.css 	new file:   mottslanding.jpg 	new file:   waterTrailsPanel.php

---
+++ waterTrailsPanel.php
@@ -0,0 +1,43 @@
+<style type="text/css">
+#centerContainer{
+	width:606px;	
+}
+#trailImage{
+	width: 250px;
+	float: left;
+	padding: 10px;
+}
+#trailName{
+	float: left;
+	width:250px;
+}
+#paddleRating{
+	float: right;
+	width: 356px;
+}
+#trailContent{
+	width: 100%;
+}
+</style>
+
+<html>
+<body>
+<div id='centerContainer'>
+	<div id='trailHeader'>
+	<div id='trailName'>
+		<h2>Motts Landing to City Docs</h2>
+		<p>Near Fredericksburg, Virginia</p>
+	</div>
+	<div id='paddleRating'>
+		<p>paddle rating</p>
+	</div>
+	</div>
+	<div id='trailContent'>
+	<div id='trailContent'>
+		<img id='trailImage' src='mottslanding.jpg' />
+		<p>Tracing a 40-mile route on two tributary waterways of the Chesapeake Bay, paddling adventures await as you explore a vast and changing landscape, spanning 13,000 years of human history, past 10,000 acres of public lands dedicated to resource conservation in northern Virginia. Interpretive exhibits located at eight access points present a tapestry of time and place — past, present and future — each reveals a different facet of an extraordinary resource.</p>
+
+		<p>The 20 mile stretch of the Upper Segment begins on free-flowing, tree-lined Bull Run, which widens as it joins the Occoquan River, opening to an expansive, freshwater lake formed by the Occoquan Reservoir dam. The Lower Segment’s brackish, open waters pass marinas and protected marshlands along the Mason Neck Peninsula, on the wide expanse of the tidal Potomac River.</p>
+	</div>
+</body>
+</html>
