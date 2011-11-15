7b6208897d50376245cb513f325e06af7916107c
Ford Peterson <fordpeterson@gmail.com>
2011-11-15 13:33:15 -0500

    worked on trailsPanel

--- waterTrailsPanel.php
+++ waterTrailsPanel.php
@@ -1,43 +1,90 @@
 <style type="text/css">
 #centerContainer{
-	width:606px;	
+	width:606px;
+	
+}
+#trailHeader{
+	width:100%;
+	padding: 10px;
+	text-align: center;
+	position:relative;
+}
+#trailName{
+	float: left;
+	width: 40%;
 }
 #trailImage{
 	width: 250px;
 	float: left;
 	padding: 10px;
 }
-#trailName{
-	float: left;
-	width:250px;
-}
 #paddleRating{
 	float: right;
-	width: 356px;
+	text-align:center;
 }
 #trailContent{
 	width: 100%;
 }
+#paddleRating th{
+	padding-left: 10px;
+	padding-right:10px;
+	font-size:18px;	
+}
+#paddleRating td{
+	text-align: center;	
+}
+#trailLinks{
+	width: 100%;
+}	
+#links{
+	float: left;	
+}
+
 </style>
 
 <html>
 <body>
-<div id='centerContainer'>
-	<div id='trailHeader'>
-	<div id='trailName'>
-		<h2>Motts Landing to City Docs</h2>
-		<p>Near Fredericksburg, Virginia</p>
+<div id="centerContainer">
+<div id="trailHeader">
+		<div id="trailName">
+			<h1>Trail Name</h1>
+        	<h3>Location, City</h3>
+		</div>
+        <div id="paddleRating">
+        	<table>
+            	<th>Length</th>
+                <th>Class</th>
+                <th>Scenery</th>
+                <th>Camping</th>
+                <tr>
+                	<td>Yes</td>
+                    <td>3*</td>
+                    <td>Good</td>
+                    <td>Yes</td>
+                </tr>
+            </table>
+            <p>* Class varies with water conditions<br />
+            Be sure to check water contidions with USGS</p>
+    </div>
+  </div>
+  <div id="trailContent">
+  	<img name="" src="" id="trailImage">
+    <p>Estimated Trip Time: 5 hours with Lunch</p>
+  	<p>Tracing a 40-mile route on two tributary waterways of the Chesapeake Bay, paddling adventures await as you explore a vast and changing landscape, spanning 13,000 years of human history, past 10,000 acres of public 		lands dedicated to resource conservation in northern Virginia. Interpretive exhibits located at eight access points present a tapestry of time and place — past, present and future — each reveals a different facet of an extraordinary resource.</p>
+	<p>The 20 mile stretch of the Upper Segment begins on free-flowing, tree-lined Bull Run, which widens as it joins the Occoquan River, opening to an expansive, freshwater lake formed by the Occoquan Reservoir dam. The Lower 	Segment’s brackish, open waters pass marinas and protected marshlands along the Mason Neck Peninsula, on the wide expanse of the tidal Potomac River.</p>
 	</div>
-	<div id='paddleRating'>
-		<p>paddle rating</p>
-	</div>
-	</div>
-	<div id='trailContent'>
-	<div id='trailContent'>
-		<img id='trailImage' src='mottslanding.jpg' />
-		<p>Tracing a 40-mile route on two tributary waterways of the Chesapeake Bay, paddling adventures await as you explore a vast and changing landscape, spanning 13,000 years of human history, past 10,000 acres of public lands dedicated to resource conservation in northern Virginia. Interpretive exhibits located at eight access points present a tapestry of time and place — past, present and future — each reveals a different facet of an extraordinary resource.</p>
-
-		<p>The 20 mile stretch of the Upper Segment begins on free-flowing, tree-lined Bull Run, which widens as it joins the Occoquan River, opening to an expansive, freshwater lake formed by the Occoquan Reservoir dam. The Lower Segment’s brackish, open waters pass marinas and protected marshlands along the Mason Neck Peninsula, on the wide expanse of the tidal Potomac River.</p>
-	</div>
+    <div id="trailLinks">
+   	  <div id="links">
+		<table>
+    			<th>Helpful Links</th>
+     		   <tr><td>USGS at closest location</td></tr>
+     		   <tr><td>Local River orginazition</td></tr>
+    		    <tr><td>Maps</td></tr>
+		  </table>
+   	  </div>
+      
+  </div>
+  </div>
+</div>
 </body>
 </html>
