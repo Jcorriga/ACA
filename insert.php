8141de7b8dfd863abf45aad2903028ef30214e8b
Ford Peterson <fordpeterson@gmail.com>
2011-12-07 07:54:08 -0500

    edit insert

--- insert.php
+++ insert.php
@@ -1,3 +1,5 @@
+
+
 <?php
 	include('db_connect.php');
 /*	
@@ -7,6 +9,23 @@
   	}
 
 */  
+
+	$name = $_POST[trailName];
+	echo $name . "br/";
+	$location = $_POST[location];
+	$state = $_POST[state];
+	$length = $_POST[trailLength];
+	$class = $_POST[class1];
+	$scenery = $_POST[scenery1];
+	$camping = $_POST[camping1];
+	$tripTime = $_POST[tripTime];
+	$description = $_POST[description];
+//	$trailImage = $_POST[trailImage]
+	$river = $_POST[river];
+	$startLocation = $_POST[trailStart];
+	$endLocation = $_POST[trailEnd];
+	$usgsLink = $_POST[usgsLink];
+	
   	$sql="INSERT INTO waterTrail 
   			(name, location, state, length, class, scenery, camping, tripTime,
   			description, trailImage, river, startLocation, endLocation, usgsLink)
@@ -15,10 +34,15 @@
 	'$_POST[class1]','$_POST[scenery1]','$_POST[camping1]','$_POST[tripTime]',
 	'$_POST[description]', 'imageUrl','$_POST[river]','$_POST[trailStart]',
 	'$_POST[trailEnd]','$_POST[usgsLink]')";
-	echo $sql;
-	$results = mysqli_query($db, $sql) or die("Error!!!");
+	$result = mysqli_query($db, $sql) or die("Error!!!");
+	$query = "SELECT * FROM waterTrail w WHERE w.name='$name'";
+	$result = mysqli_query($db, $query) or die("Error 2!!!");
+	$row = mysqli_fetch_array($result);
+	$index = $row['index'];
+	echo $index;
 
-	echo "1 trail added";
+	//	mysql_close($db);
+	echo $id;
 
-mysql_close($db);
+
 ?>
